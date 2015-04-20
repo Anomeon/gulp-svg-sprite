37241cd791ea7aa0d69f20b173aab28a19a09ccc
Adel Khamatov <hamatovadel@gmail.com>
2015-04-20 20:12:25 +0300

    Configure grunt

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,51 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    svgstore = require('gulp-svgstore'),
+    svgmin = require('gulp-svgmin'),
+    rimraf = require('gulp-rimraf'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    runSequence = require('run-sequence');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('svg-min', function() {
+  return gulp.src('input/*.svg')
+    .pipe(svgmin({
+        js2svg: {
+            pretty: true
+        }
+    }))
+    .pipe(gulp.dest('output/svgo'));
+
+});
+
+gulp.task('svg-store', function() {
+  return gulp.src('output/svgo/*.svg')
+    .pipe(rename({prefix: 'icon-'}))
+    .pipe(svgstore({
+        js2svg: {
+            pretty: true
+        }
+    }))
+    .pipe(gulp.dest('output/svgo/'));
+
+});
+
+gulp.task('svg-rename', function() {
+  gulp.src("output/svgo/svgo.svg")
+    .pipe(rename("icons-svg.svg"))
+    .pipe(gulp.dest("output"));
+});
+
+
+gulp.task('watch', function () {
+  gulp.watch('input/*.svg', ['default']);
+});
+
+gulp.task('clean', function (cb) {
+  return del([
+    'output/svgo/*.svg'
+  ], cb);
+});
+
+gulp.task('default', function (cb){
+  runSequence('clean','svg-min', 'svg-store','svg-rename','watch', cb);
 });
